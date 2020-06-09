e01f01467f19a3c41a9d7d0ac05a82b62fd0c527
MUDASSIRMunir <58942002+MUDASSIRMunir@users.noreply.github.com>
2020-06-09 14:57:04 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import introduction from './Introduction';
+import Introduction from './Introduction';
+import Title from './Title';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />,<title />,<introduction />
+    <App />,<Title />,<Introduction />
   </React.StrictMode>,
   document.getElementById('root')
 );
